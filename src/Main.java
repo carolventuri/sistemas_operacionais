1a3f275cea3a50336dfd5c159cb56074ca89a89d
carolventuri <carolventuri@gmail.com>
2024-03-18 11:29:56 -0300

    Atualização do código realizada em aula - FCFS

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
-
 import java.util.Random;
-
 public class Main {
 
     static int MAXIMO_TEMPO_EXECUCAO = 65535;
-
-    static int n_processos = 3;
-    int[] id = new int[n_processos];
-
+    static int n_processos = 3; //é preciso definir o número de processos pois ele será o número de posições do vetor
 
     public static void main(String[] args) {
-
-
 
         int[] tempo_execucao = new int[n_processos];
         int[] tempo_chegada = new int[n_processos];
         int[] prioridade = new int[n_processos];
         int[] tempo_espera = new int[n_processos];
-        int[] tempo_restante = new int[n_processos];
+        int[] tempo_restante = new int[n_processos]; //vetor auxiliar para armazenar o tempo restante de cada processo ==vai decrementando do tempo de execução
 
 
         Scanner teclado = new Scanner (System.in);
@@ -33,11 +26,13 @@ public class Main {
 
         while(true) {
             System.out.print("Escolha o argoritmo?: [1=FCFS 2=SJF Preemptivo 3=SJF Não Preemptivo  4=Prioridade Preemptivo 5=Prioridade Não Preemptivo  6=Round_Robin  7=Imprime lista de processos 8=Popular processos novamente 9=Sair]: ");
+
+
             alg =  teclado.nextInt();
 
 
             if (alg == 1) { //FCFS
-                FCFS(tempo_execucao, tempo_espera, tempo_restante, tempo_chegada);
+                FCFS(tempo_execucao, tempo_espera, tempo_restante);
             }
             else if (alg == 2) { //SJF PREEMPTIVO
                 SJF(true, tempo_execucao, tempo_espera, tempo_restante, tempo_chegada);
@@ -69,7 +64,6 @@ public class Main {
 
             }
         }
-
     }
 
     public static void popular_processos(int[] tempo_execucao, int[] tempo_espera, int[] tempo_restante, int[] tempo_chegada,  int [] prioridade ){
@@ -83,7 +77,7 @@ public class Main {
         for (int i = 0; i < n_processos; i++) {
             //Popular Processos Aleatorio
             if (aleatorio == 1){
-                tempo_execucao[i] = random.nextInt(10)+1;
+                tempo_execucao[i] = random.nextInt(10)+1; //10 é o limite máximo, coloco +1 porque não quero que o tempo seja zero
                 tempo_chegada[i] = random.nextInt(10)+1;
                 prioridade[i] = random.nextInt(15)+1;
             }
@@ -97,7 +91,6 @@ public class Main {
                 prioridade[i] = teclado.nextInt();
             }
             tempo_restante[i] = tempo_execucao[i];
-
         }
     }
 
@@ -113,18 +106,42 @@ public class Main {
     public static void imprime_stats (int[] espera) {
         int[] tempo_espera = espera.clone();
         //Implementar o calculo e impressão de estatisticas
+        int tempo_espera_total = 0;
+
+        for (int i=0; i<n_processos; i++){
+            // System.out.println("Processo ["+i+"]: tempo_espera="+);   VER ESSSA LINHHA NO CODIGO DO PROFE
+            tempo_espera_total += tempo_espera[i];
+        }
 
     }
 
-    public static void FCFS(int[] execucao, int[] espera, int[] restante, int[] chegada){
+    public static void FCFS(int[] execucao, int[] espera, int[] restante){
+        //clones dos parâmetros de entrada, para não alterar os valores do vetor inicial
         int[] tempo_execucao = execucao.clone();
         int[] tempo_espera = espera.clone();
         int[] tempo_restante = restante.clone();
-        int[] tempo_chegada = chegada.clone();
+        int processo_em_execucao = 0; //processo inicial no FIFO é zero
 
-        //implementar código do FCFS
-        //...
-        //
+        //implementar código do FCFS:
+        for (int i=0; i<MAXIMO_TEMPO_EXECUCAO; i++){
+            System.out.println ("tempo["+i+"]: processo["+processo_em_execucao+"] restante="+tempo_restante[processo_em_execucao]);
+
+            if (tempo_execucao[processo_em_execucao]==tempo_restante [processo_em_execucao]){
+                tempo_espera[processo_em_execucao] = i-1;
+            }
+
+            if (tempo_restante [processo_em_execucao] ==1){
+                if (processo_em_execucao== (n_processos-1)){
+                    break;
+                }
+                else{
+                    processo_em_execucao++;
+                }
+            }
+            else{
+                tempo_restante [processo_em_execucao]--;
+            }
+        }
 
         imprime_stats(tempo_espera);
     }
